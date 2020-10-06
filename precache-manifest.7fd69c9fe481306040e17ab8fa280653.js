3e5759c432ad147ab93c8391157343e76d776f11
Mariano89 <marianoe1989@gmail.com>
2020-10-05 18:11:36 -0700

    Updates

---
+++ precache-manifest.7fd69c9fe481306040e17ab8fa280653.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e1652395a0ca7a4d59113c9aad9ecb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc65cace5b8954526a90",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f2680d3c8abe7deb80f2",
+    "url": "/static/js/2.d31d81d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d31d81d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc65cace5b8954526a90",
+    "url": "/static/js/main.2bcc50f6.chunk.js"
+  },
+  {
+    "revision": "82392d60d0a8223a0b6b",
+    "url": "/static/js/runtime-main.17b0a5e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
