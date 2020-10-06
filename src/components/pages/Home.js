332fedb4a3e91aeba699062ca545360e5a2df84e
Mariano89 <marianoe1989@gmail.com>
2020-10-05 18:44:27 -0700

    Create folder structure + install sass

---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <main id="home">
+      <div className="home-container">
+        <p>Meagan Steinkamp</p>
+      </div>
+    </main>
+  )
+}
+
+export default Home;
