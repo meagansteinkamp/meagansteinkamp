c31cf6b1c02dbae4fa8be00cd640e5bc03c856ec
Mariano89 <marianoe1989@gmail.com>
2020-12-29 21:35:42 -0800

    Add mobile nav menu lines between links

--- src/components/common/NavMenu.js
+++ src/components/common/NavMenu.js
@@ -1,6 +1,9 @@
 import React, { useContext } from 'react';
 import Logo from '../../images/logo.png';
 import X from '../../images/x.png';
+import NavLine1 from '../../images/nav_line_1.png';
+import NavLine2 from '../../images/nav_line_2.png';
+import NavLine3 from '../../images/nav_line_3.png';
 import { NavMenuContext } from '../../contexts/NavMenuContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
@@ -11,9 +14,9 @@ const NavMenu = () => {
   return (
     <div className={navMenuActive ? 'nav-menu active' : 'nav-menu'}>
       <div className="logo-button-container">
-        <div className="logo-container">
+        {/*<div className="logo-container">
           <img src={Logo} className="logo" alt="Meagan Steinkamp Logo" />
-        </div>
+        </div>*/}
 
         <div className="close-menu-button" onClick={()=> toggleNavMenu()}>
           <img src={X} className="close-button" alt="Close Nav Menu" />
@@ -21,21 +24,24 @@ const NavMenu = () => {
       </div>
 
       <div className="nav-links-container">
-        <ul className="nav-links">
-          <li>about</li>
-          <li>contact</li>
-          <li>resume</li>
-          <li className="icon">
-            <a href="https://www.instagram.com/meagan.png/" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon className="social-icon" icon={faInstagram} alt="Instagram" />
-            </a>
-          </li>
-          <li className="icon">
-            <a href="https://www.linkedin.com/in/meagan-steinkamp-24707b50/" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon className="social-icon" icon={faLinkedin} alt="LinkedIn" />
-            </a>
-          </li>
-        </ul>
+          <p>about</p>
+          <img className="nav-line" src={NavLine1} />
+          <p>contact</p>
+          <img className="nav-line" src={NavLine2} />
+          <p>resume</p>
+          <img className="nav-line" src={NavLine3} />
+          <div className="icons-container">
+            <div className="icon">
+              <a href="https://www.instagram.com/meagan.png/" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon className="social-icon" icon={faInstagram} alt="Instagram" />
+              </a>
+            </div>
+            <div className="icon">
+              <a href="https://www.linkedin.com/in/meagan-steinkamp-24707b50/" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon className="social-icon" icon={faLinkedin} alt="LinkedIn" />
+              </a>
+            </div>
+          </div>
       </div>
     </div>
   )
